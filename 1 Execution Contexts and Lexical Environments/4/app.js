74bc5554015111c61a6160fa86531f7fab9fdcd5
kirandash <thekirankumardash@gmail.com>
2017-03-23 21:48:36 +0530

    1-4 Execution Context - Creation and Hoisting

---
+++ 1 Execution Contexts and Lexical Environments/4/app.js
@@ -0,0 +1,18 @@
+console.log(a);
+b();
+
+//b and a is stored in memory space but a is not assigned the 'Hello World' value, while in case of function the entire function is stored in memory
+
+//EXECUTION CONTEXT is created in 2 phases - 1. CREATION PHASE - Creation of global object this and the outer environment, Setup Memory spces for variables and functions "HOISTING" - Functions are stored entirely but variables are stored without assignment
+
+//Phase 2 - EXECUTION PHASE - code looks at the memory space and then starts running top to bottom.
+
+var a = 'Hello world!';
+
+function b () {
+	console.log('function b is called');
+}
+
+//So better to use the variable and function after they are defined
+/*console.log(a);
+b();*/
