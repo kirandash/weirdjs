8862a2a7b5d99d2a622e1c31bf3d93e1d36b7293
kirandash <thekirankumardash@gmail.com>
2017-03-30 22:28:36 +0530

    3-13 White space

---
+++ 3 Functions n Objects/13whitespace/app.js
@@ -0,0 +1,18 @@
+//White spaces are ignored in JS
+var 
+	//Comment1
+	firstName,
+	//Comment2
+	middleName,
+	//Comment3
+	lastName;
+
+var 
+	person = {
+		//Comment4
+		firstname: 'Kiran',
+		//Comment5
+		lastname: 'Dash'
+	}
+
+console.log(person);
